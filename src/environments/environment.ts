5a4179b9913dec1566d890075d3b9c5185c8ceb5
SterbenXIII <36611723+SterbenXIII@users.noreply.github.com>
2021-05-29 16:25:15 +0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,7 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyDrS0-unmXFVqvXAfO8itR_HvW2zZRcYZc",
-    authDomain: "reactchat-3d82f.firebaseapp.com",
-    databaseURL: "https://reactchat-3d82f.firebaseio.com",
-    projectId: "reactchat-3d82f",
-    storageBucket: "reactchat-3d82f.appspot.com",
-    messagingSenderId: "306384535474",
-    appId: "1:306384535474:web:62cfd5d70852a4f0536eab",
-    measurementId: "G-HFJH34TBNE"
+
 
   },
 };
